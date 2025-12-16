431744523166a589b3f62970d5322c630a6675aa
vulps <vulps@os-dev-vm.vulps.co.uk>
2025-12-16 08:53:58 +0000

    Update package template

--- build.sh
+++ build.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 set -e
+
+# Compile the binary
 if gcc "say.c" -static -o "say"; then
     echo "Build successful: say"
 else
     echo "Build failed"
     exit 1
 fi
+
+# Create package structure
+mkdir -p package/bin
+cp "say" package/bin/
+cp manifest.json package/
+
+# Create .vpk archive
+cd package
+tar czf "../say.vpk" *
+cd ..
+
+echo "Package created: say.vpk"
